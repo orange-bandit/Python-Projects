2c102f7bc3eab391ddd68eb10ac8b13a255dae81
orangebandit0whiteferrari <fastcarsmadmen>
2024-10-12 12:42:16 +0100

    added functionality to the shopping cart

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,24 +1,50 @@
 
-foods = []
-prices = []
+stock = {'pizza':15.00,
+         'nachos': 12.50,
+         'pop-corn':10.00,
+         'potatoes': 20.00,
+         'frost': 9.99,
+         'ratbread': 4.99,
+         'catbread': 6.99,
+         'cornbread': 30.00,
+         'bread': 60.00,
+         'metalwater': 14.99}
+
+cart = []
+
 total = 0
 
+print('----- MENU -----')
+for key, value in stock.items():
+    print(f'{key:10}: ${value:.2f}')
+
+
+
 while True:
-    food = input('Enter you preferred food purchase: (Q to Quit)')
-    if food == 'Q' or food == 'q':
+    print('----------------')
+    print(f'-empty to empty cart,-cart to show cart contents,-quit to quit program,')
+    choice = str(input(f'Enter your choice: ')).lower()
+
+    if choice == ('quit' or '-quit'):
+        print('----------------')
+        print('goodbye')
         break
-    else:
-        price = int(input(f'Enter the price for {food}: $'))
-        foods.append(food)
-        prices.append(price)
-
-
-print('----- Your Cart -----')
-
-for food in foods:
-    print(food)
-
-for price in prices:
-    total += price
-
-print(f'Your total is: ${total}')
+    elif choice == ('cart' or '-cart'):
+        print('-- Your Cart ---')
+        print(f'{cart}',end=' ')
+        for item in cart:
+            total += float(stock.get(item))
+        print('----------------')
+        total = round(total,3)
+        print(f'your total is ${total:.2f}')
+    elif choice == ('empty' or '-empty'):
+        cart.clear()
+        total = 0
+        print('----------------')
+        print('emptied cart')
+    elif stock.get(choice) is not None:
+        cart.append(choice)
+        
+        print(f'Added {choice} to your cart')
+    elif stock.get(choice) is None:
+        print(f'{choice} is not in stock')
